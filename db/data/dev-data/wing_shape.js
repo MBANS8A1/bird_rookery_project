ac0517601763e81082f7dd2aeb7d319595d5b813
MBANS8A1 <seanmbng@aol.com>
2023-11-23 21:09:18 +0000

    added some input bird data and images

---
+++ db/data/dev-data/wing_shape.js
@@ -0,0 +1,35 @@
+//shape_d will be here (have to create this table after order and family and then after this table create birds)
+
+module.exports = [
+
+    {
+        shape_name:"Passive soaring wings",
+        description:"These wings have long broad feathers that are spread out integrated wing Sslots for warm air to rise through (creating \'thermals\') that help the bird rise even higher in the sky. This mechanism of maintaining altitude helps the bird to conserve energy and circle on any prey spotted.",
+        image:
+        //eagles hawks cranes vultures
+    },
+    {
+        shape_name:"Active soaring wings",
+        description:"These wings are long and narrow and there are no wing slots; these wings allow higher soaring. Birds with these wings depend more on the flow and direction of wind currents meaning they do not need to flap as often. Due to this, these birds fly over oceans and large bodies of water where there a constant strong wind currents.",
+        image:
+        //albatrosses, gannets
+    } 
+    ,  
+    {
+        shape_name:"Elliptical wings",
+        description:"These wings are adapted for generating bursts of high speed, but involves a lot of necessary flapping in order to do so. These wings are short and broad with have large wing slots. Birds with these wings take off and land quickly and can make sharp maneuvers and acrobatics in the process. These wings help birds catch prey but also makes them able to dodge other larger bird predators. "
+        image:
+        //crows, ravens, blackbirds, sparrows, and thrushes
+    },
+    {
+        shape_name:"High-speed wings",
+        description:"These wings are long and thin but not as long as active soaring wings. Birds with these wings are very fast as the name implies. The wings are tapered and the are no wing slots and birds with these wings can hunt their prey in the air as well as dive and swoop quickly to catch their prey "
+        image:
+        //falcons, swifts, swallows, and terns
+    },
+    {
+        shape_name:"Hovering wings",
+        description:"These are small but quick wings shaped around adapted blood vessels and nerves to help create incredibly fast movements. These wings are good for birds that sip nectar like the Ruby-throated Hummingbird."
+        //falcons, swifts, swallows, and terns
+    } 
+]
