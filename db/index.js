e607eef49c3c82dfcb509a3aaf35ff7667959ab7
MBANS8A1 <seanmbng@aol.com>
2024-03-07 11:42:32 +0000

    added hosting code to javascript files, package.json and created a .env.production file; all related to API and PostgreSQL database hosting on ElephantSQL and Render

--- db/index.js
+++ db/index.js
@@ -1,14 +1,22 @@
 const {Pool} = require('pg')
 
-//select right environment(test vs development)
+//select right environment(test vs development vs production)
 
-const ENV = process.env.NODE_ENV || "development"
+const ENV = process.env.NODE_ENV || "production"
 const pathToEnvFile = `${__dirname}/../.env.${ENV}`
 require("dotenv").config({path: pathToEnvFile})
 const PGDATABASE = process.env.PGDATABASE
 
-if(!process.env.PGDATABASE){
-    throw new Error('PGDATABASE has not been configured and set to process.env global object API ')
+const configObj = {}
+
+if(ENV === "production"){
+    configObj.connectionString = process.ENV.DATABASE_URL
+    configObj.max = 3
+
 }
 
-module.exports = new Pool()
+if(!process.env.PGDATABASE && !process.env.DATABASE_URL){
+    throw new Error('PGDATABASE has not been configured and set to process.env global object API or DATABASE URL has not been set.')
+}
+
+module.exports = new Pool(configObj)
--- listen.js
+++ listen.js
@@ -1,7 +1,10 @@
 const app = require('./app.js')
-const PORT = 3000
+
+const { PORT = 9091 } = process.env;
 
 app.listen(PORT,(err)=>{
-    if(err) console.log(err)
+    if(err){ console.log(err)
+    }else{
     console.log(`Server listening on port ${PORT}...`)
+    }
 })
